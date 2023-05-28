f6633a0de32ee899896b25ec8e9152eacbaae87f
Plaza521 <alex_platonov1@mail.ru>
2023-05-28 15:53:00 +0300

    Repair generator (now work)

--- field.py
+++ field.py
@@ -17,7 +17,12 @@ class Field:
         self
     ) -> None:
         self.field = \
-            [[cell.Cell()] * self.field_size for _ in range(self.field_size)]
+            [
+                [None] * self.field_size for _ in range(self.field_size)
+            ]
+        for i in range(self.field_size):
+            for j in range(self.field_size):
+                self.field[j][i] = cell.Cell()
         mines_left = self.mines_amount
         while mines_left:
             x = randint(0, self.field_size - 1)
@@ -25,12 +30,21 @@ class Field:
             if self.field[y][x].is_mine:
                 mines_left += 1
                 continue
+            self.field[y][x].is_mine = True
             for i in range(-1, 2):
                 for j in range(-1, 2):
                     rx = x + i
                     ry = y + j
                     if rx < 0 or ry < 0 or \
-                       rx > self.field_size or ry > self.field_size:
+                       rx >= self.field_size or ry >= self.field_size:
                         continue
-                    self.field[y][x].mines_around += 1
+                    self.field[ry][rx].mines_around += 1
             mines_left -= 1
+
+    def __str__(self) -> str:
+        out = ""
+        for line in self.field:
+            for field_cell in line:
+                out += f"||`{field_cell}`||"
+            out += '\n'
+        return out[:-1]
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import field
 def main() -> None:
     minesweeper_field = field.Field(9, 10)
     minesweeper_field.generate()
-    print(minesweeper_field.field)
+    print(minesweeper_field)
 
 
 if __name__ == '__main__':
